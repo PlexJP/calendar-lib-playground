4dc9edb8468caec43bc6238e2b69927c891fbe85
tetty0217 <tetty0217@users.noreply.github.com>
2024-02-26 19:15:45 +0000

    deploy: 81c41f59c22260703b7a768ee0ab3882528a8c7a

--- _next/static/uLTA9eIU6Xxx2mXEOzOSo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/436a1112dc9b46a2.css","static/chunks/pages/index-cdcd29fc3c400851.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
