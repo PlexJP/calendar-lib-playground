d0d9a957ab14a225ae174b6ca0e56bc63f538023
tetty0217 <dev.tetty@gmail.com>
2024-02-27 03:48:45 +0900

    chore: pages router適用

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: false,
-  images: {
-    unoptimized: true
-  },
-  output: "export"
-}
--- src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "../../styles/globals.css";
-
-function MyApp({ component, pageProps }) {
-	return <Component {...pageProps} />;
-}
-
-export default MyApp;
